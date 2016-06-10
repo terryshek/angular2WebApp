4efdf01625f5fd5fde32d91f50aeb0ce55f19aef
terryshek <terryshek@msn.com>
2016-06-10 11:37:56 +0800

    add login logic with loading button

--- app/login.component.ts
+++ app/login.component.ts
@@ -16,7 +16,7 @@ import { UsernameValidator } from './validation/userValidator'
   selector: 'login-form',
   providers:[AuthService],
   template: `
-    <div class="container" style="padding-top: 80px">
+    <div class="container" style="padding-top: 80px" >
       <form [ngFormModel]="form" class="login-screen text-center" novalidate>
         <h2>Login</h2>
         <hr>
@@ -24,18 +24,21 @@ import { UsernameValidator } from './validation/userValidator'
             <span class="input-group-addon" id="basic-addon1">@</span>
             <input type="text" class="form-control" ngControl="username" placeholder="Username" aria-describedby="basic-addon1" required>
         </div>
-        <div *ngIf="username.dirty && !username.valid" class="alert alert-danger" style="margin: 5px">
+        <div *ngIf="username.dirty && !username.valid && !username.pending" class="alert alert-danger" style="margin: 5px">
             <p *ngIf="username.errors.required"><strong>Username is required.</strong></p>
         </div>
          <div class="input-group pass">
            <span class="input-group-addon" id="basic-addon1"><i class="glyphicon glyphicon-link"></i></span>
             <input type="PASSWORD" class="form-control" ngControl="password" placeholder="Password" aria-describedby="basic-addon1">
         </div>
-        <div *ngIf="password.dirty && !password.valid" class="alert alert-danger" style="margin: 5px">
+        <div *ngIf="password.dirty && !password.valid && !password.pending" class="alert alert-danger" style="margin: 5px">
             <p *ngIf="password.errors.required"><strong>Password is required.</strong></p>
         </div>
         <hr/>
-        <button type="submit" (click)="submitData()" [disabled]="!form.valid"class="btn btn-default">Login</button>
+        <button type="submit" (click)="login()" [disabled]="!form.valid" class="btn btn-default">
+          <div [hidden]="loggedIn">Login</div>
+          <div [hidden]="!loggedIn"><span class="glyphicon glyphicon-refresh glyphicon-refresh-animate"></span>&nbsp; loading ...</div>
+        </button>
       </form>
     </div>
     	`,
@@ -43,14 +46,15 @@ import { UsernameValidator } from './validation/userValidator'
 })
 
 export class LoginComponent implements OnInit {
+  public loggedIn :boolean;
 
   ngOnInit():any {
     console.log("LoginComponent")
+    console.log(this.loggedIn)
   }
 
   constructor(private _service:AuthService, private _router: Router, private builder: FormBuilder) {
-
-
+    this.loggedIn = _service.isLoggedin
     this.username = new Control('', Validators.required, UsernameValidator.checkIfAvailable);
     this.password = new Control('', Validators.required);
 
@@ -65,11 +69,14 @@ export class LoginComponent implements OnInit {
   }
 
   login() {
-    this._service.loginfn(this.localUser).then((res) => {
+    this.loggedIn = true;
+    this._service.loginfn(this.form.value).then((res) => {
       if(res)
         this._router.navigate(['Dashboard']);
       else
         console.log(res);
+
+      this.loggedIn = false;
     })
   }
 
--- app/service/loginService.ts
+++ app/service/loginService.ts
@@ -1,29 +1,34 @@
 import {Injectable} from '@angular/core';
 import {Http, Headers} from '@angular/http';
-
+import { Observer } from 'rxjs/Observer';
+import 'rxjs/add/operator/share';
 
 @Injectable()
 export class AuthService {
-  isLoggedin: boolean;
+  isLoggedin: boolean = false;
 
   constructor(private _http:Http) {
 
   }
 
   loginfn(usercreds) {
-    this.isLoggedin = false;
     var headers = new Headers();
-    var creds = 'username=' + usercreds.username + '&password=' + usercreds.password;
+    var creds = JSON.stringify({username:usercreds.username, password:usercreds.password});
 
     headers.append('Content-Type', 'application/json');
+    headers.append('Access-Control-Allow-Origin', '*');
 
     return new Promise((resolve) => {
 
-      this._http.post('http://103.44.162.106:8337/login?', creds, {headers: headers}).subscribe((data) => {
-          if(data.json().success) {
-            console.log(data.json())
-            window.localStorage.setItem('auth_key', data.json().token);
-            this.isLoggedin = true;}
+      this._http.post('http://103.44.162.106:8337/login', creds, {headers: headers})
+        .map(res => res.json())
+        .subscribe((res) => {
+          console.log(res)
+          if(res.status == 200) {
+            //console.log(data.json())
+            window.localStorage.setItem('this_id', res.this_profile["_id"]);
+            this.isLoggedin = true;
+          }
           resolve(this.isLoggedin)
         }
       )
